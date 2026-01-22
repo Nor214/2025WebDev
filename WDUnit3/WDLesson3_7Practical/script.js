266f50dd3f78295c481ec129a33e7105e01ce1bf
Nor214 <NorrisR4@nycstudents.net>
2026-01-22 14:30:14 +0000

    d

--- WDUnit3/WDLesson3_7Practical/script.js
+++ WDUnit3/WDLesson3_7Practical/script.js
@@ -3,12 +3,42 @@
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
+function Distance(){
+      let x1 = parseFloat(document.getElementById("x1").value);
+      let y1 = parseFloat(document.getElementById("y1").value);
+      let x2 = parseFloat(document.getElementById("x2").value);
+      let y2 = parseFloat(document.getElementById("y2").value);
+      let output = document.getElementById("outputD")
 
+      let First = Math.pow(x2 - x1, 2);
+      let Second = Math.pow(y2 - y1, 2);
+
+      let D = Math.sqrt(First + Second)
+
+      output.innerHTML = D
+
+}
 /* Challenge 4: Create a function to serve as the event handler for the compound interest UI. Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
+
+function interest(){
+      let P = parseFloat(document.getElementById("P").value)
+      let R = parseFloat(document.getElementById("N").value)
+      let N = parseFloat(document.getElementById("N").value)
+      let T = parseFloat(document.getElementById("T").value)
+      let outputC = document.getElementById("outputC")
+
+      let Paranthesis = 1 + (R / N)
+
+      let Para2 = Math.pow(Paranthesis, (N * T))
+
+      let FA = P * Para2
+
+      outputC.innerHTML = FA
+}
 
 
 
