e2e821f96c610101c4ceef168755cd77f571ac36
Nor214 <NorrisR4@nycstudents.net>
2026-04-28 13:32:25 +0000

    Searches

--- WDLesson6_7Practical/script.js
+++ WDLesson6_7Practical/script.js
@@ -15,9 +15,76 @@ async function startup(){
         build +=    `<p>Vehicle_2 Contributor: ${Collision.contributing_factor_vehicle_2}</p>`
         build +=    `<hr>`
         build +=    `<p>Vehicle_1 Type: ${Collision.vehicle_type_code1}</p>`
-        build +=    `<p>Vehicle_2 Type: ${Collision.vehicle_type_code1}</p>`
+        build +=    `<p>Vehicle_2 Type: ${Collision.vehicle_type_code2}</p>`
+        build +=    `<hr>`
+        build +=    `<p>Total People Injured: ${Collision.number_of_persons_injured}</p>`
+        build +=    `<p>Total People Killed: ${Collision.number_of_persons_killed}</p>`
+        build +=    `<hr>`
+        build +=    `<p>${Collision.crash_date}</p>`
+        build +=    `<p>${Collision.crash_time}</p>`
         build += `</div>`
     }
 
     output.innerHTML = build
 }
+
+function SearchByStreet(){
+    let Street = document.getElementById("Street").value;
+    let output = document.getElementById("output");
+    let build = "";
+
+    for(let i = 0; i < data.length; i+=1){
+        let Collision = data[i]
+        if(Collision.on_street_name == Street){
+            build += `<div class="card">`
+            build +=    `<h2>${Collision.on_street_name}</h2>`
+            build +=    `<p>${Collision.off_street_name}</p>`
+            build +=    `<hr>`
+            build +=    `<p>Vehicle_1 Contributor: ${Collision.contributing_factor_vehicle_1}</p>`
+            build +=    `<p>Vehicle_2 Contributor: ${Collision.contributing_factor_vehicle_2}</p>`
+            build +=    `<hr>`
+            build +=    `<p>Vehicle_1 Type: ${Collision.vehicle_type_code1}</p>`
+            build +=    `<p>Vehicle_2 Type: ${Collision.vehicle_type_code2}</p>`
+            build +=    `<hr>`
+            build +=    `<p>Total People Injured: ${Collision.number_of_persons_injured}</p>`
+            build +=    `<p>Total People Killed: ${Collision.number_of_persons_killed}</p>`
+            build +=    `<hr>`
+            build +=    `<p>${Collision.crash_date}</p>`
+            build +=    `<p>${Collision.crash_time}</p>`
+            build += `</div>`
+            }
+    }
+
+    output.innerHTML = build
+}
+
+function SearchByVehicle(){
+    let Vehicle = document.getElementById("Vehicle").value;
+    let output = document.getElementById("output");
+    let build = "";
+
+    for(let i = 0; i < data.length; i+=1){
+        let Collision = data[i]
+        if(Collision.vehicle_type_code1 == Vehicle || Collision.vehicle_type_code2 == Vehicle){
+            build += `<div class="card">`
+            build +=    `<h2>${Collision.on_street_name}</h2>`
+            build +=    `<p>${Collision.off_street_name}</p>`
+            build +=    `<hr>`
+            build +=    `<p>Vehicle_1 Contributor: ${Collision.contributing_factor_vehicle_1}</p>`
+            build +=    `<p>Vehicle_2 Contributor: ${Collision.contributing_factor_vehicle_2}</p>`
+            build +=    `<hr>`
+            build +=    `<p>Vehicle_1 Type: ${Collision.vehicle_type_code1}</p>`
+            build +=    `<p>Vehicle_2 Type: ${Collision.vehicle_type_code2}</p>`
+            build +=    `<hr>`
+            build +=    `<p>Total People Injured: ${Collision.number_of_persons_injured}</p>`
+            build +=    `<p>Total People Killed: ${Collision.number_of_persons_killed}</p>`
+            build +=    `<hr>`
+            build +=    `<p>${Collision.crash_date}</p>`
+            build +=    `<p>${Collision.crash_time}</p>`
+            build += `</div>`
+            }
+    }
+
+    output.innerHTML = build
+}
+
